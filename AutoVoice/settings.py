cefb418cebff9c0fca353d8deb67440b4d69304e
czh2774 <9366741@qq.com>
2019-09-07 14:13:23 +0800

    增加了数据库格式严格检查

--- AutoVoice/settings.py
+++ AutoVoice/settings.py
@@ -79,7 +79,8 @@ DATABASES = {
         'PASSWORD': 'Leng9s9bxs',
         'HOST': 'localhost',
         'PORT': '3306',
-        'OPTIONS': {'charset': 'utf8mb4'},
+        'OPTIONS': {'charset': 'utf8mb4',
+                    "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
